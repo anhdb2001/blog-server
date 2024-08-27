28d3c738e8935f7200e3301792a0cc1642eab0bc
Dương Bảo Anh <167161363+anhdb2001@users.noreply.github.com>
2024-08-27 14:05:27 +0000

    update

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -1,35 +1,57 @@
-const express = require("express")
-const Post = require("../db/postModel")
-const router = express.Router()
+const express = require("express");
+const Post = require("../db/postModel");
+const router = express.Router();
 
-router.post("/posts", async(request, response) => {
-    const post = new Post(request.body);
-    try {
-        await post.save()
-        response.send(post)
-    }catch(error){
-        response.status(500).send(error)
+router.post("/posts", async (request, response) => {
+  const post = new Post(request.body);
+  try {
+    await post.save();
+    response.send(post);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+router.get("posts", async (request, response) => {
+  try {
+    const posts = await Post.find({});
+    response.send(posts);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+router.get("/post/:title", async (request, response) => {
+  try {
+    const post = await Post.findOne({ title: request.params.title });
+    response.send(post);
+  } catch (error) {
+    response.status(500).send({ error });
+  }
+});
+
+router.patch("/post/:title", async (request, response) => {
+  try {
+    const post = await Post.findByIdAndUpdate(
+      request.params.title,
+      request.body
+    );
+    await post.save();
+    response.send(post);
+  } catch (error) {
+    response.status(500).send({ error });
+  }
+});
+
+router.delete("/post/:title", async (request, response) => {
+  try {
+    const post = await Post.findByIdAndDelete(request.params.title);
+    if (!post) {
+      return response.status(404).send("Post wasn't found");
     }
-})
-
-router.get("posts", async(request, response) => {
-    try{
-        const posts = await Post.find({})
-        response.send(posts)
-    }catch(error){
-        response.status(500).send(error)
-    }
-})
-
-router.get("/post/:slug", async(request, response) => {
-    try{
-        const posts = await Post.findOne({slug: request.params.slug})
-        response.send(posts)
-    }catch(error){
-        response.status(500).send(error)
-    }
-})
-
-
-
-module.exports = router
+    response.status(204).send();
+  } catch (error) {
+    response.status(500).send({ error });
+  }
+});
+module.exports = router;
